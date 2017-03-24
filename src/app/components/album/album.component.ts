fc68231f5652cd378430618c96cf6d344d24d76f
Jacke20 <jackeaik@hotmail.com>
2017-03-24 01:33:59 +0100

    Footer template and artist page updates

--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -3,16 +3,31 @@
 
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute }    from '@angular/router';
+import { SpotifyService }    from "../../services/spotify.service";
+
 @Component({
   selector: 'app-album',
   templateUrl: './album.component.html',
-  styleUrls: ['./album.component.css']
+  styleUrls: ['./album.component.css'],
+  providers: [SpotifyService]
 })
 export class AlbumComponent implements OnInit {
+  albums = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private spotifyService: SpotifyService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.spotifyService.getAlbumsForArtist(params['id']).subscribe(
+        data => this.albums = data.items
+      );
+
+
+      this.spotifyService.getAlbumsForArtist(params['id']).subscribe(
+        data => console.log(data)
+      );
+    });
   }
 
 }
