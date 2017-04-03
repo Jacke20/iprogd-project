12492b29e9350a5ad7f3c0163e31099c21b09ea1
teorikraften <adrian.lokk94@gmail.com>
2017-04-04 00:10:56 +0200

    reviews are now added to both reviews and user

--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -21,7 +21,7 @@ export class ReviewService {
  	}
 
  	addReviewForUser(userID, objectToAdd): void {
- 		this.af.database.object('users/' + userID + '/reviews/' + objectToAdd.artist)
+ 		this.af.database.object('users/' + userID + '/reviews/')
  			.set(objectToAdd);
  	}
 }
